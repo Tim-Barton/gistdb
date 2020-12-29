0e1770a3e8ec916dd1e923e95183608b96fca4a7
Tim Barton <timothyebarton@gmail.com>
2020-12-29 17:26:48 +1000

    Added the GistDb as an interface to allow downstream projects to automock

--- interfaces.go
+++ interfaces.go
@@ -6,6 +6,13 @@ import (
 	"github.com/google/go-github/github"
 )
 
+type GistDb interface {
+	Insert(string, string) error
+	UpdateOne(string, string) error
+	UpdateMany(map[string]string) error
+	FindOne(string) (string, error)
+}
+
 type GithubGists interface {
 	Get(context.Context, string) (*github.Gist, *github.Response, error)
 	List(context.Context, string, *github.GistListOptions) ([]*github.Gist, *github.Response, error)
