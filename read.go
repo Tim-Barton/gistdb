514957faf6d8bd28ab758572753ce8bb21cb50ca
Tim Barton <timothyebarton@gmail.com>
2020-12-29 12:11:35 +1000

    Update the FineOne read function`

--- read.go
+++ read.go
@@ -7,9 +7,9 @@ import (
 )
 
 //FindOne returns the single named file from the gist if it exists. Returns an error file doesn't exist
-func (c Connection) FindOne(filename string) (*string, error) {
+func (c Connection) FindOne(filename string) (string, error) {
 	if !isFilenameInGistFileList(filename, c.gist.Files) {
-		return nil, fmt.Errorf("Filename %s not found in Gist", filename)
+		return "", fmt.Errorf("Filename %s not found in Gist", filename)
 	}
-	return c.gist.Files[github.GistFilename(filename)].Content, nil
+	return *c.gist.Files[github.GistFilename(filename)].Content, nil
 }
